048765135c42fd55c7962ee460d97a87ea70af83
Stephan Sum <sum@live.de>
2019-02-27 15:11:02 +0100

    Added autofac DI; Lots of refactoring;

--- GithubBackup/BackupService.cs
+++ GithubBackup/BackupService.cs
@@ -9,40 +9,50 @@ namespace GithubBackup
     public class BackupService
     {
 
-        private string Username { get; set; }
-        private string Password { get; set; }
-        private string Destination { get; set; }
+        public string Destination { get; set; }
+        public Credentials Credentials { get; set; }
 
-        public BackupService()
+        public BackupService(Credentials credentials, string destination)
         {
-            var client = PromptForCredentials();
-            DisplayUserData(client);
-
-            var repos = GetAllRepositoriesAsync(client);
-            PromptForDestinationFolder();
-            BackupAllRepositories(repos, client, Destination);
+            Destination = destination;
+            Credentials = credentials;
         }
 
-        private IReadOnlyList<Repository> GetAllRepositoriesAsync(GitHubClient client)
+        public IReadOnlyList<Repository> GetRepos()
         {
+            var client = CreateGithubClient(Credentials);
             var task = client.Repository.GetAllForCurrent();
             task.Wait();
             var repos = task.Result;
-
-            foreach (var repo in repos)
-            {
-                Console.WriteLine($"{repo.Name}");
-                Console.WriteLine($"{repo.CloneUrl}");
-            }
             return repos;
         }
 
-        private void BackupAllRepositories(IReadOnlyList<Repository> repos, GitHubClient client, string destinationRootFolder)
+        public void CloneRepos(IReadOnlyList<Repository> repos)
         {
             var cloneOptions = new LibGit2Sharp.CloneOptions();
 
             cloneOptions.CredentialsProvider = (url, user, cred)
-                => new LibGit2Sharp.UsernamePasswordCredentials { Username = Username, Password = Password };
+                => new LibGit2Sharp.UsernamePasswordCredentials { Username = Credentials.Login, Password = Credentials.Password };
+
+            Console.WriteLine($"Starting to clone all repos to {Destination}");
+
+            Parallel.ForEach(repos, (repo) =>
+            {
+                var repoDestination = Path.Combine(Destination, repo.Name);
+                Console.WriteLine($"Starting to clone {repo.Name}");
+                LibGit2Sharp.Repository.Clone(repo.CloneUrl, repoDestination, cloneOptions);
+                Console.WriteLine($"Finished to clone {repo.Name}");
+            });
+
+            Console.WriteLine($"Finished cloning all {repos.Count} repos.");
+        }
+
+        public void CloneRepos(IReadOnlyList<Repository> repos, string token, string destinationRootFolder)
+        {
+            var cloneOptions = new LibGit2Sharp.CloneOptions();
+
+            cloneOptions.CredentialsProvider = (url, user, cred)
+                => new LibGit2Sharp.UsernamePasswordCredentials { Username = token, Password = string.Empty };
 
             Console.WriteLine($"Starting to clone all repos to {destinationRootFolder}");
 
@@ -57,32 +67,24 @@ namespace GithubBackup
             Console.WriteLine($"Finished cloning all {repos.Count} repos.");
         }
 
-        private GitHubClient PromptForCredentials()
+        public void DisplayUserData()
         {
-            Console.WriteLine("Username?");
-            Username = Console.ReadLine();
-
-            Console.WriteLine("Password?");
-            Password = Console.ReadLine();
-
-            var client = new GitHubClient(new ProductHeaderValue(Username));
-            var basicAuth = new Credentials(Username, Password);
-            client.Credentials = basicAuth;
-
-            return client;
-        }
-
-        private void PromptForDestinationFolder()
-        {
-            Console.WriteLine("Destination?");
-            Destination = Console.ReadLine();
-        }
-
-        private async void DisplayUserData(GitHubClient client)
-        {
-            var user = await client.User.Current();
+            var client = CreateGithubClient(Credentials);
+            var userTask = client.User.Current();
+            userTask.Wait();
+            var user = userTask.Result;
 
             Console.WriteLine($"Hello {user.Name} (Id: {user.Id}),");
         }
+
+
+        private GitHubClient CreateGithubClient(Credentials credentials)
+        {
+            var client = new GitHubClient(new ProductHeaderValue(credentials.Login));
+            client.Credentials = credentials;
+            return client;
+        }
+
+
     }
 }
---
+++ GithubBackup/Commands/CredentialCmdWrapper.cs
@@ -0,0 +1,43 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using Octokit;
+using System;
+
+namespace GithubBackup
+{
+    public class CredentialCmdWrapper
+    {
+
+        public CommandLineApplication ParentCommand { get; set; }
+        public CommandLineApplication Command { get; set; }
+        public Func<Credentials, string, BackupService> BackupServiceFactory { get; set; }
+        public Func<string, string, AuthenticationType, Credentials> CredentialsFactory { get; set; }
+
+
+        public CredentialCmdWrapper(
+            CommandLineApplication parentCommand, 
+            Func<Credentials, string, BackupService> backupServiceFactory,
+            Func<string, string, AuthenticationType, Credentials> credentialsFactory)
+        {
+            ParentCommand = parentCommand;
+            BackupServiceFactory = backupServiceFactory;
+            CredentialsFactory = credentialsFactory;
+
+
+            Command = ParentCommand.Command("credential-based", (credentialBasedCmd) =>
+            {
+                credentialBasedCmd.Description = "Creates a github backup. Authentification is done via user credentials.";
+                var userNameArgument = credentialBasedCmd.Argument("Username", "Your git username or mail.").IsRequired();
+                var passwordArgument = credentialBasedCmd.Argument("Password", "Your git password.").IsRequired();
+                var destinationArgument = credentialBasedCmd.Argument("Destination", "The destination folder for the backup.")
+                                            .Accepts(v => v.ExistingDirectory());
+
+                credentialBasedCmd.OnExecute(() =>
+                {
+                    var credentials = CredentialsFactory(userNameArgument.Value, passwordArgument.Value, AuthenticationType.Basic);
+                    var backupService = BackupServiceFactory(credentials, destinationArgument.Value);
+                });
+            });
+        }
+
+    }
+}
