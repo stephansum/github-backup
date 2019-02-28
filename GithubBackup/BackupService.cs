3f50beb68d8d95c1d58162aa284980de0b295618
BlackMesaCode <admin@blackmesa.eu>
2019-02-28 15:40:57 +0100

    Added shell progress bar;

--- GithubBackup/BackupService.cs
+++ GithubBackup/BackupService.cs
@@ -1,6 +1,8 @@
 ﻿using Octokit;
+using ShellProgressBar;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -14,7 +16,8 @@ namespace GithubBackup
 
         public BackupService(Credentials credentials, string destination)
         {
-            Destination = destination;
+            var backupFolderName = $"Backup [{DateTime.Now.ToString("yyyy-MM-dd HH-mm", CultureInfo.InvariantCulture)}]";
+            Destination = Path.Combine(destination, backupFolderName);
             Credentials = credentials;
         }
 
@@ -27,24 +30,68 @@ namespace GithubBackup
             return repos;
         }
 
+        private Dictionary<string, Exception> _exceptions = new Dictionary<string, Exception>();
+
         public void CloneRepos(IReadOnlyList<Repository> repos)
         {
-            var cloneOptions = new LibGit2Sharp.CloneOptions();
-
-            cloneOptions.CredentialsProvider = (url, user, cred)
-                => new LibGit2Sharp.UsernamePasswordCredentials { Username = Credentials.Login, Password = Credentials.Password };
-
             Console.WriteLine($"Starting to clone all repos to {Destination}");
+
+            var rootProgressBarOptions = new ProgressBarOptions
+            {
+                ForegroundColor = ConsoleColor.Green,
+                BackgroundColor = ConsoleColor.DarkGreen,
+                ProgressCharacter = '─',
+                CollapseWhenFinished = false,
+                EnableTaskBarProgress = true,
+            };
+
+            var rootProgressBar = new ProgressBar(repos.Count, "Cloning");
 
             Parallel.ForEach(repos, (repo) =>
             {
-                var repoDestination = Path.Combine(Destination, repo.Name);
-                Console.WriteLine($"Starting to clone {repo.Name}");
-                LibGit2Sharp.Repository.Clone(repo.CloneUrl, repoDestination, cloneOptions);
-                Console.WriteLine($"Finished to clone {repo.Name}");
+                var repoDestination = Path.Combine(Destination, repo.FullName);
+
+                ChildProgressBar progressBar = null;
+
+                var cloneOptions = new LibGit2Sharp.CloneOptions();
+                cloneOptions.OnTransferProgress = (progress) =>
+                {
+                    if (progressBar == null)
+                        progressBar = rootProgressBar.Spawn(progress.TotalObjects, repo.Name, new ProgressBarOptions {
+                            CollapseWhenFinished = true,  ForegroundColorDone = ConsoleColor.Green, ForegroundColor = ConsoleColor.Yellow });
+
+                    progressBar.Tick(progress.ReceivedObjects);
+                    return true;
+                };
+
+                cloneOptions.RepositoryOperationCompleted = (context) =>
+                {
+                    progressBar?.Dispose();
+                };
+
+                cloneOptions.CredentialsProvider = (url, user, cred)
+                    => new LibGit2Sharp.UsernamePasswordCredentials { Username = Credentials.Login, Password = Credentials.Password };
+                try
+                {
+                    LibGit2Sharp.Repository.Clone(repo.CloneUrl, repoDestination, cloneOptions);
+                }
+                catch (Exception ex)
+                {
+                    _exceptions[repo.FullName] = ex;
+                }
+
+                rootProgressBar.Tick();
             });
 
+            rootProgressBar.Dispose();
+
             Console.WriteLine($"Finished cloning all {repos.Count} repos.");
+            foreach (var repoName in _exceptions.Keys)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Error while cloning {repoName}:");
+                Console.WriteLine(_exceptions[repoName]);
+            }
         }
 
         public void CloneRepos(IReadOnlyList<Repository> repos, string token, string destinationRootFolder)
