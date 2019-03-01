54a5608d53cd21b63ef04970d2d40cdf90e0ab14
BlackMesaCode <admin@blackmesa.eu>
2019-03-01 16:13:24 +0100

    Refactored bootstrapper;

--- GithubBackup/Program.cs
+++ GithubBackup/Program.cs
@@ -9,33 +9,14 @@ namespace GithubBackup
 {
     class Program
     {
-        // https://github.com/natemcmaster/CommandLineUtils
-        // https://github.com/iamarcel/dotnet-core-neat-console-starter
-        // https://gist.github.com/iamarcel/8047384bfbe9941e52817cf14a79dc34
-        // https://gist.github.com/iamarcel/9bdc3f40d95c13f80d259b7eb2bbcabb
-
         static int Main(string[] args)
         {
-            var builder = new ContainerBuilder();
-
-            // Registering types within the assembly:
-            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly()).AsSelf();
-            //builder.RegisterType<BackupService>().AsSelf();
-            //builder.RegisterType<GithubBackupCmdWrapper>().AsSelf();
-            //builder.RegisterType<CredentialCmdWrapper>().AsSelf();
-            //builder.RegisterType<TokenCmdWrapper>().AsSelf();
-
-            // Registering types of 3rd party assemblies
-            builder.RegisterType<Credentials>().AsSelf();
-            builder.RegisterGeneratedFactory<CredentialSubCommand.CredentialsFactoryDelegate>(new TypedService(typeof(Credentials)));
-
-            var container = builder.Build();
-            var githubBackupCmdWrapper = container.Resolve<BackupCommand>();
-
             int result = -1;
 
             try
             {
+                SetupIocContainer();
+                var githubBackupCmdWrapper = _container.Resolve<BackupCommand>();
                 result = githubBackupCmdWrapper.Command.Execute(args);
             }
             catch (Exception ex)
@@ -44,6 +25,22 @@ namespace GithubBackup
             }
 
             return result;
+        }
+
+        private static IContainer _container;
+
+        public static void SetupIocContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            // Registering types within the assembly:
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly()).AsSelf();
+
+            // Registering types of 3rd party assemblies
+            builder.RegisterType<Credentials>().AsSelf();
+            builder.RegisterGeneratedFactory<CredentialSubCommand.CredentialsFactoryDelegate>(new TypedService(typeof(Credentials)));
+
+            _container = builder.Build();
         }
     }
 }
